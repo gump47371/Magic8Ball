4059dd84919744990017796046f5a4477581fe1c
Brent <gump47371@gmail.com>
2013-06-22 16:42:38 -0400

    Added changes to Fortune.java

--- src/com/example/magic8ball/Fortune.java
+++ src/com/example/magic8ball/Fortune.java
@@ -1,16 +1,25 @@
 package com.example.magic8ball;
 
+import java.util.Random;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class Fortune extends Activity {
+public class Fortune extends ListActivity implements OnClickListener {
 
+	Button fate;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fortune);
+		shaken();
+		fate.setOnClickListener(this);
 	}
 
 	@Override
@@ -18,8 +27,19 @@ public class Fortune extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.fortune, menu);
 		return true;
+		
 	}
 	
-	final Button button = (Button) findViewById(R.id.button1);
+	private void shaken() {
 		
+		fate = (Button) findViewById(R.id.button1);
+		
+	}
+	
+	public void onClick(View view) {
+		
+		
+		String[] array = context.getResources().getStringArray(R.array.answers);
+		String randomStr = array[new Random().nextInt(array.length)];
+	}
 }
